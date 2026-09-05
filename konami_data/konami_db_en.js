604baf9d33f15842e2d0562f2205d6ed820a73ca
GitHub Action <action@github.com>
2026-09-05 08:29:15 +0000

    Auto updated 13918 cards, by daominah/yugioh_card_editor GitHub action at 2026-09-05 15:29:15.141 +07:00

--- konami_data/konami_db_en.js
+++ konami_data/konami_db_en.js
@@ -1,4 +1,4 @@
-// CardDatabase was updated at 2026-09-04T15:54:32+07:00
+// CardDatabase was updated at 2026-09-05T15:29:13+07:00
 // by github.com/daominah/yugioh_card_editor/cmd/add-card-password
 const CardDatabase = [
 	{
